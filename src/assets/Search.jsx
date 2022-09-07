8f7b486881d9c63de99850a00c90b1eb1af9a690
J4WEB <prakharsrivastava841@gmail.com>
2022-09-07 19:45:52 +0530

    integrated date-range-picker functionality

---
+++ src/assets/Search.jsx
@@ -0,0 +1,28 @@
+import React ,{useState} from 'react'
+import './Search.css'
+import {DateRangePicker} from "react-date-range"
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+//Date picker component
+
+function Search() {
+    const [startDate,setStartDate]=useState(new Date());
+    const [endDate,setEndDate]=useState(new Date());
+    const selectionRange={
+        startDate:startDate,
+        endDate:endDate,
+        key:"selection"
+    }
+    function handleChange(ranges)
+    {
+        setStartDate(ranges.selection.startDate);
+        setEndDate(ranges.selection.endDate);
+    }
+  return (
+    <div className="search">
+        <DateRangePicker ranges={[selectionRange]} onChange={handleChange} />
+    </div>
+  )
+}
+
+export default Search
